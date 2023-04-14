defc34b1053c874c013fd3e2c678d6b38718ff59
Arthur Carmello <arthurcarmello@gmail.com>
2023-04-14 12:44:36 -0300

    proj To_Do

--- Project_To_Do/JS/scripts.js
+++ Project_To_Do/JS/scripts.js
@@ -46,26 +46,27 @@ const toggleForms = () => {
 
 const updateTodo = (text) => {
 
-   const todos = document.querySelectorAll(".todo")
+    const todos = document.querySelectorAll(".todo")
 
-   todos.forEach((todo) => {
-      
-       let todoTitle = todo.querySelector("h3")
-         
-       if(todoTitle.innerText === oldInputValue) {
-          todoTitle.innerText = text;
-       }
+    todos.forEach((todo) => {
+
+        let todoTitle = todo.querySelector("h3")
+
+        if (todoTitle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+        }
     }
-)};
+    )
+};
 
 // Eventos
 todoForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+    e.preventDefault();
 
-  const inputValue = todoInput.value;
+    const inputValue = todoInput.value;
 
-    if(inputValue) {
-       saveTodo(inputValue);
+    if (inputValue) {
+        saveTodo(inputValue);
     }
 });
 
@@ -74,19 +75,19 @@ document.addEventListener("click", (e) => {
     const parentEl = targetEl.closest("div");
     let todoTitle;
 
-    if(parentEl && parentEl.querySelector("h3")) {
-       todoTitle = parentEl.querySelector("h3").innerText;
+    if (parentEl && parentEl.querySelector("h3")) {
+        todoTitle = parentEl.querySelector("h3").innerText;
     }
 
-    if(targetEl.classList.contains("finish-todo")) {
-       parentEl.classList.toggle("done");
+    if (targetEl.classList.contains("finish-todo")) {
+        parentEl.classList.toggle("done");
     }
 
-    if(targetEl.classList.contains("remove-todo")) {
+    if (targetEl.classList.contains("remove-todo")) {
         parentEl.remove();
     }
- 
-    if(targetEl.classList.contains("edit-todo")) {
+
+    if (targetEl.classList.contains("edit-todo")) {
         toggleForms();
 
         editInput.value = todoTitle;
@@ -102,12 +103,12 @@ cancelEditBtn.addEventListener("click", (e) => {
 
 editForm.addEventListener("submit", (e) => {
 
-   e.preventDefault()
+    e.preventDefault()
 
-   const editInputValue = editInput.value
+    const editInputValue = editInput.value
 
-   if(editInputValue) {
-     updateTodo(editInputValue)
-   }
-   toggleForms()
+    if (editInputValue) {
+        updateTodo(editInputValue)
+    }
+    toggleForms()
 });
